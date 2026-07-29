db6cb88cda199fb3999c628ab4b45802102d2e8e
THARA HUOKAING <tharahuokaing@gmail.com>
2026-07-29 16:04:03 +0700

    Update wedding audio source file name

--- script1.js
+++ script1.js
@@ -6,7 +6,7 @@
  * Audio Engine Configuration
  * Uses the local wedding audio asset path
  */
-const weddingAudioSrc = "huokaingthara1.mp3";
+const weddingAudioSrc = "huokaingthara.mp3";
 
 // Create HTMLAudioElement instance
 const weddingAudio = new Audio(weddingAudioSrc);
