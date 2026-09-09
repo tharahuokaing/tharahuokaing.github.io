e4ed5c1ec9f3b1e51c37502a06bf7cd434efc327
THARA HUOKAING <tharahuokaing@gmail.com>
2026-09-09 19:30:43 +0700

    Change wedding audio source to ninja_cyber_rap.mp3

--- music.js
+++ music.js
@@ -6,7 +6,7 @@
  * Audio Engine Configuration
  * Uses the local wedding audio asset path
  */
-const weddingAudioSrc = "cambodia_pride.mp3";
+const weddingAudioSrc = "ninja_cyber_rap.mp3";
 
 // Create HTMLAudioElement instance
 const weddingAudio = new Audio(weddingAudioSrc);
